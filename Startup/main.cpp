b0c473f68dd65cb04e665ef37c5e410f8e070bcb
Haneul Kim <haneul.kim.dev@gmail.com>
2023-12-12 16:12:45 +0900

    Update my::Update function parameter to use io.DeltaTime

--- Startup/main.cpp
+++ Startup/main.cpp
@@ -230,7 +230,7 @@ int main(int, char**) {
           ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
       ImGui::SetItemAllowOverlap();
 
-      my::Update(ImGui::GetIO().DeltaTime);
+      my::Update(io.DeltaTime);
       my::DoTest();
 
       ID3D11ShaderResourceView* textureView = nullptr;
