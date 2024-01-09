d323451058b6676a013419b0dd391deaf4e98deb
Haneul Kim <haneul.kim.dev@gmail.com>
2024-01-09 14:40:40 +0900

    Update particle shader to use structured input

--- MyEngine/hlsl/PS_Particle_Simple.hlsl
+++ MyEngine/hlsl/PS_Particle_Simple.hlsl
@@ -1,6 +1,13 @@
 #include "Header_Particle.hlsli"
 
-float4 main(Vertex input) : SV_TARGET
+struct PS_PARTICLE_INPUT
+{
+    float4 pos : SV_POSITION;
+    nointerpolation float size : SIZE;
+    nointerpolation uint color : COLOR;
+};
+
+float4 main(PS_PARTICLE_INPUT input) : SV_TARGET
 {
 	return float4(1.0f, 1.0f, 1.0f, 1.0f);
 }
