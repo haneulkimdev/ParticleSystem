d921185be0324d338e5cec94cf1678ed7a042db1
Haneul Kim <haneul.kim.dev@gmail.com>
2024-01-12 12:50:37 +0900

    Update shader compilation in ShaderCompile.bat

--- MyEngine/ShaderCompile.bat
+++ MyEngine/ShaderCompile.bat
@@ -4,12 +4,11 @@ if not exist "hlsl\objs\\" (
 
 :: VS
 fxc /E main /T vs_5_0 ./hlsl/VS_ParticleSystem.hlsl /Fo ./hlsl/objs/VS_ParticleSystem
-fxc /E VSQuad /T vs_5_0 ./hlsl/VS_QUAD.hlsl /Fo ./hlsl/objs/VS_QUAD
 
 :: PS
 fxc /E main /T ps_5_0 ./hlsl/PS_ParticleSystem.hlsl /Fo ./hlsl/objs/PS_ParticleSystem
-fxc /E PS_RayMARCH /T ps_5_0 ./hlsl/PS_RayMARCH.hlsl /Fo ./hlsl/objs/PS_RayMARCH
 
 :: CS
+fxc /E main /T cs_5_0 ./hlsl/CS_ParticleSystem_KickoffUpdate.hlsl /Fo ./hlsl/objs/CS_ParticleSystem_KickoffUpdate
 fxc /E main /T cs_5_0 ./hlsl/CS_ParticleSystem_Emit.hlsl /Fo ./hlsl/objs/CS_ParticleSystem_Emit
-fxc /E main /T cs_5_0 ./hlsl/CS_ParticleSystem_Update.hlsl /Fo ./hlsl/objs/CS_ParticleSystem_Update
+fxc /E main /T cs_5_0 ./hlsl/CS_ParticleSystem_Simulate.hlsl /Fo ./hlsl/objs/CS_ParticleSystem_Simulate
