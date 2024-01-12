8c11f5998f83e962397b1aa950b7132a6c287744
Haneul Kim <haneul.kim.dev@gmail.com>
2024-01-12 17:18:35 +0900

    Commented out unnecessary code in PS_ParticleSystem.hlsl

--- MyEngine/hlsl/PS_ParticleSystem.hlsl
+++ MyEngine/hlsl/PS_ParticleSystem.hlsl
@@ -9,7 +9,7 @@ struct PixelIn
 // https://en.wikipedia.org/wiki/Smoothstep
 float smootherstep(float x, float edge0 = 0.0f, float edge1 = 1.0f)
 {
-  // Scale, and clamp x to 0..1 range
+    // Scale, and clamp x to 0..1 range
     x = clamp((x - edge0) / (edge1 - edge0), 0, 1);
 
     return x * x * x * (3 * x * (2 * x - 5) + 10.0f);
@@ -17,7 +17,8 @@ float smootherstep(float x, float edge0 = 0.0f, float edge1 = 1.0f)
 
 float4 main(PixelIn pin) : SV_TARGET
 {
-    float dist = length(float2(0.5, 0.5) - pin.texCoord) * 2;
-    float scale = smootherstep(1 - dist);
-    return float4(pin.color.rgb * scale, 1.0);
+    //float dist = length(float2(0.5, 0.5) - pin.texCoord) * 2;
+    //float scale = smootherstep(1 - dist);
+    //return float4(pin.color.rgb * scale, 1.0);
+    return float4(pin.color.rgb, 1.0);
 }
