4425f943d9e0d6b19c64746eef71f7acde0123b2
Haneul Kim <haneul.kim.dev@gmail.com>
2024-01-22 14:49:35 +0900

    Add CreateBox function to GeometryGenerator

--- MyEngine/GeometryGenerator.h
+++ MyEngine/GeometryGenerator.h
@@ -7,6 +7,8 @@
 namespace my {
 class GeometryGenerator {
  public:
+  MeshData CreateBox(float width, float height, float depth);
+
   MeshData CreateSphere(float radius, UINT sliceCount, UINT stackCount);
 
   MeshData CreateQuad(float x, float y, float w, float h, float depth);
