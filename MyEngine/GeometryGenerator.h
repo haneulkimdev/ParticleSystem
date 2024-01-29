1219cb3f45772f28322c3a92c2bd4651dad79824
Haneul Kim <haneul.kim.dev@gmail.com>
2024-01-29 14:02:44 +0900

    Refactor GeometryGenerator class to use static member functions

--- MyEngine/GeometryGenerator.h
+++ MyEngine/GeometryGenerator.h
@@ -10,12 +10,12 @@
 namespace my {
 class GeometryGenerator {
  public:
-  MeshData CreateBox(float width, float height, float depth);
+  static MeshData CreateBox(float width, float height, float depth);
 
-  MeshData CreateSphere(float radius, UINT sliceCount, UINT stackCount);
+  static MeshData CreateSphere(float radius, UINT sliceCount, UINT stackCount);
 
-  MeshData CreateQuad(float x, float y, float w, float h, float depth);
+  static MeshData CreateQuad(float x, float y, float w, float h, float depth);
 
-  std::vector<MeshData> LoadModel(const std::string& filename);
+  static std::vector<MeshData> LoadModel(const std::string& filename);
 };
 }  // namespace my
